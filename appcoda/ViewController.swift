185046d9d152bd95be701030c457e43003247475
eemi <eemi@imac-de-mac11-2.local>
2017-12-05 09:57:17 +0100

    Initial Commit

---
+++ appcoda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appcoda
+//
+//  Created by eemi on 05/12/2017.
+//  Copyright © 2017 eemi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
